61b960e3ed36173e23b6e7d19b5af9945b62f1cf
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2023-01-08 13:25:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import  express from "express";
+import mongoose from "mongoose";
+import chartSchema from "./chartSchema.js";
+// import data from '../visualization/src/Data.json' assert {type:"json"}
+import fs from 'fs'
+import cors from 'cors'
+import env from 'dotenv'
+
+const app=express()
+app.use(cors())
+env.config()
+
+const Port=process.env.Port||9000
+app.listen(Port,()=>console.log('app runnig'))
+app.use('/',(req,res)=>{
+
+        chartSchema.find().then((data)=>{
+            res.status(200).json(data)
+        }).catch((error)=>{
+            res.status(404).send("error message ")
+        })
+})
+
+
+const jsonData = "JSON.parse(  fs.readFileSync('','utf-8'))"
+   
+
+const uploadData=async()=>{
+      chartSchema.create(jsonData,(err,data)=>{
+        if(err){
+            console.log(err);
+        }else{
+              console.log("data successfully uploaded");
+            }
+    
+        })
+}
+
+// uploadData()
+mongoose.connect(process.env.CONNECTION_URL,{
+    useNewUrlParser:true
+})
